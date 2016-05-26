7d579c7751369da0b7af10f0c65d5d0f3e9c4014
hakozaru <t.morii@everyleaf.com>
2016-05-27 08:12:05 +0900

    validate and call back

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,4 +7,18 @@ class Book < ActiveRecord::Base
   # rails cでBook.costと実行すると、↓のクエリが発行される
   scope :cost, -> { where("price > ?", 2000) }
 
+  validates :name, presence: true, length: { maximum: 10 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+  # この条件を満たした場合のみログを記録する
+  # (条件を満たしても満たさなくても当然データは消える)
+  def low_price
+    price < 1000
+  end
+
+  # |tes|の部分は何でもいい(bookでもfooでも)
+  after_destroy if: :low_price do |tes|
+    Rails.logger.info "Book is deleted: #{tes.attributes.inspect}"
+  end
+
 end
