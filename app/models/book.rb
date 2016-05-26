b8256069ab61b38e5bbdf2fb158b7aa6e0a7e31f
hakozaru <t.morii@everyleaf.com>
2016-05-27 07:35:10 +0900

    多対多 関連

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,9 @@
 class Book < ActiveRecord::Base
   belongs_to :publisher
 
+  has_many :authors, through: :book_authors
+  has_many :book_authors
+
   # rails cでBook.costと実行すると、↓のクエリが発行される
   scope :cost, -> { where("price > ?", 2000) }
 
